85496798ba030f3a2b2def972c234b620ddb03c6
Manish Singh <manishsingh332000@gmail.com>
2022-02-19 19:28:31 +0530

    Adding carousel

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
 
 function banner() {
-  return <div>banner</div>
+  return (
+    <div className="relative ">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showIndicators={false}
+        showStatus={false}
+        interval={4000}
+      >
+        <div></div>
+      </Carousel>
+    </div>
+  )
 }
 
 export default banner
