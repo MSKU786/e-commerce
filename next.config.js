71cc269b7e7ca000c4bde612373e115d5a7ee13f
Manish Singh <manishsingh332000@gmail.com>
2022-03-12 20:02:00 +0530

    Setting up the CLI

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ module.exports = {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
     stripe_secret_key: process.env.STRIPE_SECRET_KEY,
     host: process.env.HOST,
+    webhook_secret: process.env.WEBHOOK_SECRET,
   },
   ignoreBuildErrors: true,
 }
--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,12 +1,63 @@
-import { buffer } from 'micro'
 import * as admin from 'firebase-admin'
+import { buffer } from 'stream/consumers'
 
+//Secure a connection to firebase from the backend
 const serviceAccount = require('../../admin.json')
 
-const app = !admin.apps.length ? admin.intializeApp({!admin.credential.cert(serviceAccount)})
+const app = !admin.apps.length
+  ? admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
+  : admin.app()
 
+//Establish coonection to stripe
+const stripe = require('stripe')(process.env.stripe_secret_key)
 
+const endPointSecret = process.env.webhook_secret
+
+const fullFillOrder = async (session: any) => {
+  return app
+    .firestore()
+    .collection('users')
+    .doc(session.metadata.email)
+    .collection('orders')
+    .doc(session.id)
+    .set({
+      amount: session.amount_total / 100,
+      amount_shipping: session.total_details.amount_shipping / 100,
+      imaages: JSON.parse(session.metadata.imaages),
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+      console.log(`SUCCESS : Order ${session.id} has been added to the DB`)
+    })
+}
 export default async (req: any, res: any) => {
   if (req.method == 'POST') {
+    const requestBuffer = await buffer(req)
+    const payload = requestBuffer.toString()
+    const sig = req.headers['stripe-signature']
+
+    let event
+    //Verify that event posted came from stripe
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endPointSecret)
+    } catch (err: any) {
+      return res.status(400).send(`Webhook Error: ${err?.message}`)
+    }
+
+    //Handle the checkout session completed event
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object
+
+      return fullFillOrder(session)
+        .then(() => res.status(200))
+        .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`))
+    }
   }
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
 }
