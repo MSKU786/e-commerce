2bc5ed5d7d3b6d011248fdcffe54cca2d9f90941
Manish Singh <manishsingh332000@gmail.com>
2022-03-13 12:52:24 +0530

    success page completed

---
+++ pages/success.tsx
@@ -0,0 +1,27 @@
+import { CheckCircleIcon } from '@heroicons/react/outline'
+import Header from '../components/header'
+
+export default function Success() {
+  return (
+    <>
+      <div className="h-screen bg-gray-100">
+        <Header />
+      </div>
+      <div className="max-auto max-w-screen-lg">
+        <div className="pd-10 flex flex-col bg-white">
+          <div className="mb-6 flex items-center space-x-2">
+            <CheckCircleIcon className="h-10 text-green-600" />
+            <h1>Thank You, your order has been confirmed!</h1>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <p>
+            Thank You for shopping with us, We will send a confirmation once
+            your order has been shipped
+          </p>
+          <button className="button mt-6"> Go to my orders</button>
+        </div>
+      </div>
+    </>
+  )
+}
